1393d91a0c3557a1c1037575a4a23b81027273df
zhangyanying <347915308@qq.com>
2019-08-13 19:41:50 +0800

    完成login路由的登录校验，完成动态登录功能

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,14 +1,76 @@
 import React,{ Component } from 'react';
-import { Form, Icon, Input, Button} from 'antd';
+import { Form, Icon, Input, Button, message} from 'antd';
+import axios from 'axios';
 
 import logo from './logo.png';
 
 import './index.less';
-
+//定义变量不能放在import上面，会报错
 const Item = Form.Item;
 
-export default class Login extends Component{
+class Login extends Component{
+
+  //表单校验
+  validator = (rule,value,callback) => {
+    /*callback();*/
+    /*
+      rule 通过这个参数能知道是哪个Input组件进行表单校验
+      value 校验的值是多少
+      callback
+        callback() 代表表单校验成功
+        callback(errMsg) 代表表单校验失败，失败的提示就是errMsg
+
+     */
+
+    // console.log(rule,value);
+    const name = rule.field === 'username'? '用户名': '密码';
+    const valueReg = /^\w+$/;
+    if (!value){
+      callback(`请输入${name}`)
+    }else if (value.length < 4){
+      callback(`${name}长度要大于4`)
+    }else if (value.length > 10){
+      callback(`${name}长度要小于10`)
+    }else if (!valueReg.test(value)){
+      callback(`${name}只能包含英文，字符，下划线`)
+    }
+
+    callback();
+
+  };
+
+  login = (e) => {
+    //首先要禁止button默认行为
+    e.preventDefault();
+    //只有验证成功才可以进行登录
+    this.props.form.validateFields((error,values) => {
+      // console.log(error,values);
+      //error是校验的错误信息提示，当校验成功，值为null，判断校验成功，主要判断error是否有值
+      if (!error){
+        //校验成功，允许登录，发送请求
+        axios.post('http://localhost:3000/login',values)
+          .then((response) => {
+            const result = response.data;
+            if (result.status === 0){
+              message.success('登录成功',1)
+            }else{
+              message.error(result.msg,1)
+            }
+          })
+          .catch((error) => {
+            message.error('登录失败，网络出现异常',1)
+          })
+
+      }
+      //有错误，浏览器自动提示
+    })
+  };
+
+
   render(){
+    //提取一个方法进行表单验证
+    //表单里面默认有三个属性：history，location，match
+    const { getFieldDecorator } = this.props.form;
     return <div className="login">
       <header className="login-header">
         <img src={logo} alt="logo"/>
@@ -16,17 +78,45 @@ export default class Login extends Component{
       </header>
       <section className="login-from">
         <h2>用户登录</h2>
-        <Form onSubmit={this.handelSubmit}>
+        <Form onSubmit={this.login}>
           <Item>
-            <Input prefix={<Icon type="user" />} placeholder="用户名"/>
+            {
+              getFieldDecorator(
+                'username',
+                {
+                  rules: [
+                    {validator: this.validator}
+                  ]
+                }
+              )(
+                <Input prefix={<Icon type="user" />} placeholder="用户名"/>
+              )
+            }
           </Item>
           <Item>
-            <Input prefix={<Icon type="lock" />} placeholder="用户名"/>
+            {
+              getFieldDecorator(
+                'password',
+                {
+                  rules: [
+                    {validator: this.validator}
+                  ]
+                }
+              )(
+                <Input prefix={<Icon type="lock"  />} type="password" placeholder="密码"/>
+              )
+            }
           </Item>
-          <Button type="primary" htmlType="submit" className="login-btn">登录</Button>
+          <Item>
+            <Button type="primary" htmlType="submit" className="login-btn">登录</Button>
+          </Item>
 
         </Form>
       </section>
     </div>
   }
 }
+
+//使用高阶组件，让login有form属性，可以进行表单校验
+//高阶组件，调用返回一个新组件
+export default Form.create()(Login);
